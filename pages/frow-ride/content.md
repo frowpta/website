fa370ca062f18be66c45a076e5b881a9639ace02
Drew Neil <andrew.jr.neil@gmail.com>
2023-06-21 06:45:35 +0100

    Create a basic /frow-ride page

---
+++ pages/frow-ride/content.md
@@ -0,0 +1,22 @@
+---
+title: 'The Frow Ride'
+date: '2023-06-21'
+---
+
+## Children's Cycle Event
+
+Sunday, July 16th on the Forest Way.
+
+* 9am - 12pm
+* Last entry around 11am
+* £5 entry fee
+* Prizes for children
+* Collect stamps
+* Choose your distance
+* BBQ
+* Cakes
+* Drinks
+
+Money raised goes to Forest Row Primary School.
+
+![](/images/Childrens-Cycle-Event.jpg)
---
+++ pages/frow-ride/index.tsx
@@ -0,0 +1,40 @@
+import Head from "next/head";
+import Layout from "../../components/layout";
+import { GetStaticProps, GetStaticPaths } from "next";
+import { getContent } from "../../lib/posts";
+import utilStyles from "../../styles/utils.module.css";
+import Date from '../../components/date'
+
+export const getStaticProps: GetStaticProps = async ({ params }) => {
+  const pageData = await getContent("pages/frow-ride");
+  return {
+    props: {
+      pageData,
+    },
+  };
+};
+
+export default function FrowRide({
+  pageData,
+}: {
+  pageData: {
+    title: string;
+    date: string;
+    contentHtml: string;
+  };
+}) {
+  return (
+    <Layout home>
+      <Head>
+        <title>The Frow Ride</title>
+      </Head>
+      <article>
+        <h1 className={utilStyles.headingXl}>{pageData.title}</h1>
+        <div className={utilStyles.lightText}>
+          <Date dateString={pageData.date} />
+        </div>
+        <div dangerouslySetInnerHTML={{ __html: pageData.contentHtml }} />
+      </article>
+    </Layout>
+  );
+}
