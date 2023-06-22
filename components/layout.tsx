be97df8845af72f0ab0384279de7e7ad8dc5820c
Drew Neil <andrew.jr.neil@gmail.com>
2023-06-22 17:51:12 +0100

    Remove Next.js image from og:image

--- components/layout.tsx
+++ components/layout.tsx
@@ -26,7 +26,7 @@ export default function Layout({
           property="og:image"
           content={`https://og-image.vercel.app/${encodeURI(
             siteTitle
-          )}.png?theme=light&md=0&fontSize=75px&images=https%3A%2F%2Fassets.zeit.co%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`}
+          )}.png?theme=light&md=0&fontSize=75px`}
         />
         <meta name="og:title" content={siteTitle} />
         <meta name="twitter:card" content="summary_large_image" />
